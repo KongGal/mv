d97b8c6bd82f845acffbc091cce16635adcba0de
Daniel R <danielr@bpgstudios.com>
2024-07-31 11:24:41 +0200

    update gtag

--- gtag.php
+++ gtag.php
@@ -1,9 +1,9 @@
 <!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-17CY7XZP4P"></script>
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-FQEW738C4V"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
-  gtag('config', 'G-17CY7XZP4P');
+  gtag('config', 'G-FQEW738C4V');
 </script>
