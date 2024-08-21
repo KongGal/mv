d1a113b64b567095cda1f265c4b4c917089539f1
Daniel R <danielr@bpgstudios.com>
2024-08-22 01:00:21 +0200

    Update

--- index.php
+++ index.php
@@ -39,29 +39,28 @@
 	<!--<script src="js/firefly.js" type="text/javascript"></script> -->
 	<script src="js/main.js" type="text/javascript"></script>
     <div class="center-container">
-        <blockquote class="tiktok-embed" cite="https://www.tiktok.com/@rogvirriteret/video/7397531271066406176" data-video-id="7397531271066406176" style="max-width: 605px;min-width: 325px;">
-            <section>
-                <a target="_blank" title="@rogvirriteret" href="https://www.tiktok.com/@rogvirriteret?refer=embed">@rogvirriteret</a> Det er underlige løgne men hun lyder til at tro på hun har læst det🤷🏻‍♂️
-                <a title="kludderliv_marlene" target="_blank" href="https://www.tiktok.com/tag/kludderliv_marlene?refer=embed">#kludderliv_marlene</a>
-                <a title="kludder" target="_blank" href="https://www.tiktok.com/tag/kludder?refer=embed">#kludder</a>
-                <a title="kludderliv" target="_blank" href="https://www.tiktok.com/tag/kludderliv?refer=embed">#kludderliv</a>
-                <a title="sludder" target="_blank" href="https://www.tiktok.com/tag/sludder?refer=embed">#sludder</a>
-                <a title="mændsvilkår" target="_blank" href="https://www.tiktok.com/tag/m%C3%A6ndsvilk%C3%A5r?refer=embed">#mændsvilkår</a>
-                <a title="kludderløgne" target="_blank" href="https://www.tiktok.com/tag/kludderl%C3%B8gne?refer=embed">#kludderløgne</a>
-                <a title="løgn" target="_blank" href="https://www.tiktok.com/tag/l%C3%B8gn?refer=embed">#løgn</a>
-                <a title="60000" target="_blank" href="https://www.tiktok.com/tag/60000?refer=embed">#60000</a>
-                <a title="nohatefuldhed" target="_blank" href="https://www.tiktok.com/tag/nohatefuldhed?refer=embed">#nohatefuldhed</a>
-                <a title="gudekompleks" target="_blank" href="https://www.tiktok.com/tag/gudekompleks?refer=embed">#gudekompleks</a>
-                <a title="tiktokdk" target="_blank" href="https://www.tiktok.com/tag/tiktokdk?refer=embed">#tiktokdk</a>
-                <a title="rogvi" target="_blank" href="https://www.tiktok.com/tag/rogvi?refer=embed">#rogvi</a>
-                <a title="rogvificeret" target="_blank" href="https://www.tiktok.com/tag/rogvificeret?refer=embed">#rogvificeret</a>
-                <a title="rogvicomedy" target="_blank" href="https://www.tiktok.com/tag/rogvicomedy?refer=embed">#rogvicomedy</a>
-                <a title="tiktokdk🇩🇰" target="_blank" href="https://www.tiktok.com/tag/tiktokdk%F0%9F%87%A9%F0%9F%87%B0?refer=embed">#tiktokdk🇩🇰</a>
-                <a title="hatefuldhed" target="_blank" href="https://www.tiktok.com/tag/hatefuldhed?refer=embed">#hatefuldhed</a>
-                <a title="punktstavligt" target="_blank" href="https://www.tiktok.com/tag/punktstavligt?refer=embed">#punktstavligt</a>
-                <a target="_blank" title="♬ original sound - RogviComedy" href="https://www.tiktok.com/music/original-sound-7397531309138774816?refer=embed">♬ original sound - RogviComedy</a>
-            </section>
-        </blockquote>
+    <blockquote class="tiktok-embed" cite="https://www.tiktok.com/@rogvificeret/video/7405580628302761249" data-video-id="7405580628302761249" style="max-width: 605px;min-width: 325px;" > 
+        <section> 
+            <a target="_blank" title="@rogvificeret" href="https://www.tiktok.com/@rogvificeret?refer=embed">@rogvificeret</a> 
+            <a title="kongenafhatefuldhed" target="_blank" href="https://www.tiktok.com/tag/kongenafhatefuldhed?refer=embed">#kongenafhatefuldhed</a> 
+            <a title="hatefuldhed" target="_blank" href="https://www.tiktok.com/tag/hatefuldhed?refer=embed">#hatefuldhed</a> 
+            <a title="punktstaveligt" target="_blank" href="https://www.tiktok.com/tag/punktstaveligt?refer=embed">#punktstaveligt</a> 
+            <a title="punktstaveligtalt" target="_blank" href="https://www.tiktok.com/tag/punktstaveligtalt?refer=embed">#punktstaveligtalt</a> 
+            <a title="punktstavligtalt" target="_blank" href="https://www.tiktok.com/tag/punktstavligtalt?refer=embed">#punktstavligtalt</a> 
+            <a title="nohatefuldhed" target="_blank" href="https://www.tiktok.com/tag/nohatefuldhed?refer=embed">#nohatefuldhed</a> 
+            <a title="nohate" target="_blank" href="https://www.tiktok.com/tag/nohate?refer=embed">#nohate</a> 
+            <a title="stophatenu" target="_blank" href="https://www.tiktok.com/tag/stophatenu?refer=embed">#stophatenu</a> 
+            <a title="stophatefuldhednu" target="_blank" href="https://www.tiktok.com/tag/stophatefuldhednu?refer=embed">#stophatefuldhednu</a> 
+            <a title="mobningerenholdsport" target="_blank" href="https://www.tiktok.com/tag/mobningerenholdsport?refer=embed">#mobningerenholdsport</a> 
+            <a title="rogvi" target="_blank" href="https://www.tiktok.com/tag/rogvi?refer=embed">#rogvi</a> 
+            <a title="rogvificeret" target="_blank" href="https://www.tiktok.com/tag/rogvificeret?refer=embed">#rogvificeret</a> 
+            <a title="rogvisdom" target="_blank" href="https://www.tiktok.com/tag/rogvisdom?refer=embed">#rogvisdom</a> 
+            <a title="rogvirriteret" target="_blank" href="https://www.tiktok.com/tag/rogvirriteret?refer=embed">#rogvirriteret</a> 
+            <a title="erdudermentalt" target="_blank" href="https://www.tiktok.com/tag/erdudermentalt?refer=embed">#erdudermentalt</a> 
+            <a title="rogvikingofhatefuldhed" target="_blank" href="https://www.tiktok.com/tag/rogvikingofhatefuldhed?refer=embed">#rogvikingofhatefuldhed</a> 
+            <a target="_blank" title="♬ original sound - Rogvi" href="https://www.tiktok.com/music/original-sound-7405580648851671841?refer=embed">♬ original sound - Rogvi</a> 
+        </section> 
+    </blockquote> 
         <script async src="https://www.tiktok.com/embed.js"></script>
     </div>
 </body>
